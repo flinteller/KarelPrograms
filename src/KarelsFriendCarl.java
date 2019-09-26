880c6a2222142bdecbedf11381cb677db735378c
flinteller <flinteller@ssfs.org>
2019-09-26 11:37:29 -0400

    Made own city and two robots follow instructions

--- src/KarelsFriendCarl.java
+++ src/KarelsFriendCarl.java
@@ -6,6 +6,34 @@ public class KarelsFriendCarl {
         Robot karel = new Robot(city,0, 0, Direction.SOUTH, 1);
         Robot carl = new Robot(city, 3, 0, Direction.EAST, 0);
 
+        karel.move();
+        karel.move();
+        karel.move();
+        karel.putThing();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.move();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+
+        carl.pickThing();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.move();
+        carl.putThing();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+
+
 
 
     }
